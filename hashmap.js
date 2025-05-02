7d91fbd216c0c243194f293255604b071f85d245
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-02 13:50:23 -0500

    Add example hash function

--- hashmap.js
+++ hashmap.js
@@ -1,6 +1,19 @@
 import List from "./linked-list/linked-list.js";
 
+function hash(key) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+}
+
 export class HashMap {
+  #capacity = 0;
+  #loadFactor = 1;
   #bucketIndexCheck(index) {
     if (index < 0 || index >= buckets.length) {
       throw new Error("Trying to access index out of bounds");
